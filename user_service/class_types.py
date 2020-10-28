ab1471ad43ad374da6f0ae5d388bf04303ed868c
Yifei Xu <yifeix@Yifeis-Air.hsd1.il.comcast.net>
2020-10-27 20:10:04 -0500

    1. use soft link instead of hard copy 2. combine classes for use domain and auction domain since they overlap

--- user_service/class_types.py
+++ user_service/class_types.py
@@ -9,6 +9,16 @@ class User(object):
             'password': self.password_
         }
 
+class Item_base(object):
+    def __init__(self, key, name):
+        self.key_ = key
+        self.name_ = name
+    def serialize(self):
+        return{
+            'key': self.key_,
+            'name': self.name_,
+    }
+
 class Acknowledgement(object):
     def __init__(self, success):
         self.success_ = success
@@ -23,6 +33,6 @@ class Endpoint(object):
         self.domain_ = domain
         self.ip_ = ip
         self.port_ = port
-        
+
     def get_prefix(self):
         return self.ip_+":"+str(self.port_)+"/" # e.g. http://127.0.0.1:5000/
